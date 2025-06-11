3fa4f5ad6427c78c733a0cd96afd8f9d89161982
manasvideshmukh <manasvideshmukh3@gmail.com>
2025-06-11 11:55:47 +0530

    Model evalution and MLFlow is added

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from mlProject.pipeline.stage01_data_ingestion import DataIngestionTrainingPipel
 from mlProject.pipeline.stage02_data_validation import DataValidationTrainingPipeline
 from mlProject.pipeline.stage03_data_transformation import DataTransformationTrainingPipeline
 from mlProject.pipeline.stage04_model_trainer import ModelTrainerTrainingPipeline
+from mlProject.pipeline.stage05_model_evalution import ModelEvaluationTrainingPipeline
 
 
 
@@ -46,6 +47,17 @@ STAGE_NAME = "Model Trainer stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = ModelTrainerTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Model evaluation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelEvaluationTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
